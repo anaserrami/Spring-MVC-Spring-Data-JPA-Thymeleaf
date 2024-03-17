6702af04de5cf916988b1c7f2d33dcd4cce9a603
anaserrami <anaserrami24@gmail.com>
2024-03-17 15:21:16 +0000

    add PatientController

--- src/main/java/ma/enset/hospital/web/PatientController.java
+++ src/main/java/ma/enset/hospital/web/PatientController.java
@@ -1,16 +1,28 @@
 package ma.enset.hospital.web;
 
 import lombok.AllArgsConstructor;
+import ma.enset.hospital.entities.Patient;
 import ma.enset.hospital.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class PatientController {
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(){
-        return "Patients";
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "5") int size){
+        Page<Patient> patientPage = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("listPatients", patientPage.getContent());
+        return "patients";
     }
 }
