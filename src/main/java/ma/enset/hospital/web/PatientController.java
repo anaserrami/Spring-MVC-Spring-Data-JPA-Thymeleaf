c5ba653cf84cc4ba9047e68d06678b5048efa840
anaserrami <anaserrami24@gmail.com>
2024-03-17 17:12:47 +0000

    add delete and edit patient

--- src/main/java/ma/enset/hospital/web/PatientController.java
+++ src/main/java/ma/enset/hospital/web/PatientController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -31,6 +32,16 @@ public class PatientController {
     }
     @GetMapping("/")
     public String home(){
+        return "redirect:/index";
+    }
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> patients(){
+        return patientRepository.findAll();
+    }
+    @GetMapping("/save")
+    public String save(Patient patient){
+        patientRepository.save(patient);
         return "redirect:/index";
     }
     @GetMapping("/delete")
