01775c5b1ddb6fda7001ab7a18343fc0d8c04fab
anaserrami <anaserrami24@gmail.com>
2024-03-17 14:12:52 +0000

    init commit

---
+++ src/main/java/ma/enset/hospital/HospitalApplication.java
@@ -0,0 +1,44 @@
+package ma.enset.hospital;
+
+import ma.enset.hospital.entities.Patient;
+import ma.enset.hospital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class HospitalApplication implements CommandLineRunner {
+    @Autowired
+    private PatientRepository patientRepository;
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception{
+        /*
+        // En utilisant Constructeur sans paramètre
+        Patient patient1 = new Patient();
+        patient1.setId(null);
+        patient1.setNom("Hassan");
+        patient1.setDateNaissance(new Date());
+        patient1.setMalade(false);
+        patient1.setScore(10);
+
+        // En utilisant Constructeur avec paramètre
+        Patient patient2 = new Patient(null, "Ahmed", new Date(), false, 20);
+
+        // En utilisant Builder
+        Patient patient3 = Patient.builder().nom("Karim").malade(true).score(30).build();
+        */
+
+        patientRepository.save(new Patient(null, "Hassan", new Date(), false, 10));
+        patientRepository.save(new Patient(null, "Ahmed", new Date(), false, 20));
+        patientRepository.save(new Patient(null, "Karim", new Date(), true, 30));
+
+
+    }
+}
---
+++ src/test/java/ma/enset/hospital/Activitie3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activitie3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
