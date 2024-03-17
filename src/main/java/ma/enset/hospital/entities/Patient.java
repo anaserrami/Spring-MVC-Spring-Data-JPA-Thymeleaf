23602e00d6a70e2d026d15bebacff8102d50bca3
anaserrami <anaserrami24@gmail.com>
2024-03-17 14:11:35 +0000

    add patient class

---
+++ src/main/java/ma/enset/hospital/entities/Patient.java
@@ -0,0 +1,21 @@
+package ma.enset.hospital.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Patient {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 50)
+    private String nom;
+    @Temporal(TemporalType.DATE)
+    private Date dateNaissance;
+    private boolean malade;
+    private int score;
+}
