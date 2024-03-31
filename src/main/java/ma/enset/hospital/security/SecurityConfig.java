177dd64f0fed25f86f3bfb22f41016b70328812f
anaserrami <anaserrami24@gmail.com>
2024-03-31 12:22:50 +0000

    Add Security part

---
+++ src/main/java/ma/enset/hospital/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package ma.enset.hospital.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder){
+        String encodedPassword = passwordEncoder.encode("1234");
+        System.out.println(encodedPassword);
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(encodedPassword).roles("USER").build(),
+                User.withUsername("user2").password(encodedPassword).roles("USER").build(),
+                User.withUsername("admin").password(encodedPassword).roles("USER","ADMIN").build()
+        );
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .formLogin(ar->ar.loginPage("/login").permitAll())
+                .rememberMe(Customizer.withDefaults())
+                .authorizeHttpRequests(ar->ar.requestMatchers("/webjars/**").permitAll())
+                .authorizeHttpRequests(ar->ar.requestMatchers("/deletePatient/**").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .exceptionHandling(ar->ar.accessDeniedPage("/notAuthorized"))
+                .build();
+    }
+}
---
+++ src/main/java/ma/enset/hospital/web/SecurityController.java
@@ -0,0 +1,17 @@
+package ma.enset.hospital.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+    @GetMapping("/notAuthorized")
+    public String notAuthorized() {
+        return "notAuthorized";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
